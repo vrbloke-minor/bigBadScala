a4ca9c6bf0c76ccabd87f57cf842271c19ed1cf5
vrbloke <stanislawwis@gmail.com>
2020-11-05 14:02:17 +0100

    Fixed minor mistake. Added sync method to keep redundant fields in sync

--- src/com/swsn/messingaround3/HasColor.scala
+++ src/com/swsn/messingaround3/HasColor.scala
@@ -1,20 +1,22 @@
 package com.swsn.messingaround3
-import scala.math.min
+import scala.math.{min, max}
 
 trait HasColor {
   var color: (Int, Int, Int)
   var (red, green, blue) = color
 
+  def sync(x: Int): Unit = x match {
+    case 0 => red = color._1; green = color._2; blue = color._3
+    case 1 => color = (red, green, blue)
+  }
   def setColor(r:Int,g:Int,b:Int): Unit = {
     color = (r,g,b)
-    red = r
-    green = g
-    blue = b
+    sync(0)
   }
-  def increaseRedBy(redPercentage: Float): Unit = { red = min((red + red * redPercentage).toInt, 255) }
-  def increaseGreenBy(greenPercentage: Float): Unit = { green = min((green + green * greenPercentage).toInt, 255) }
-  def increaseBlueBy(bluePercentage: Float): Unit = { blue = min((blue + blue * bluePercentage).toInt,255) }
-  def decreaseRedBy(redPercentage: Float): Unit = { red = min((red - red * redPercentage).toInt,255) }
-  def decreaseGreenBy(greenPercentage: Float): Unit = { green = min((green - green * greenPercentage).toInt,255) }
-  def decreaseBlueBy(bluePercentage: Float): Unit = { blue = min((blue - blue * bluePercentage).toInt,255) }
+  def increaseRedBy(redPercentage: Float): Unit = { red = min((red + red * redPercentage).toInt, 255); sync(1) }
+  def increaseGreenBy(greenPercentage: Float): Unit = { green = min((green + green * greenPercentage).toInt, 255); sync(1) }
+  def increaseBlueBy(bluePercentage: Float): Unit = { blue = min((blue + blue * bluePercentage).toInt,255); sync(1) }
+  def decreaseRedBy(redPercentage: Float): Unit = { red = max((red - red * redPercentage).toInt,0); sync(1) }
+  def decreaseGreenBy(greenPercentage: Float): Unit = { green = max((green - green * greenPercentage).toInt,0); sync(1) }
+  def decreaseBlueBy(bluePercentage: Float): Unit = { blue = max((blue - blue * bluePercentage).toInt,0); sync(1) }
 }
